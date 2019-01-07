c2c1baf91e7ff40b6db53130441e72eec3b60225
EleBrain <aaa@aaa>
2019-01-08 00:43:04 +0900

    タイプミスを修正

--- AbilityUpEditor/Form1.cs
+++ AbilityUpEditor/Form1.cs
@@ -3,7 +3,7 @@ namespace AbilityUpEditor {
     public partial class Form1<T> : JsonEditorForm<T> {
         public Form1() {
             FileTypeName = "能力アップデータ";  //ファイルの名前の説明
-            FileName = "AbilitypData";            //ファイルの名前
+            FileName = "AbilityUpData";            //ファイルの名前
             InitializeComponent();
             Init();
         }
